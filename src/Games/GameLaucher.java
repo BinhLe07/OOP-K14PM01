a703d6b6e6126e91aae77f8cb5b15b5dc750b314
LeDoQuocBinh_2008110078_K14DCPM01 <binhle030601@gmail.com>
2022-02-24 12:12:19 +0700

    up

---
+++ src/Games/GameLaucher.java
@@ -0,0 +1,9 @@
+package Games;
+
+public class GameLaucher {
+    public static void main(String[] args) 
+    {
+        GuessGame game = new GuessGame();
+        game.StartGame();    
+    }    
+}
---
+++ src/Games/Player.java
@@ -0,0 +1,10 @@
+package Games;
+
+public class Player {
+    int number = 0;
+    void Guess(){
+        number = (int) (Math.random()*10);
+        System.out.println("I'am guessing:  "+ number);
+    }    
+
+}
