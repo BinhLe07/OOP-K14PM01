9bb506ab8b776597e57d1a9d82faf2e3eb275b38
LeDoQuocBinh_2008110078_K14DCPM01 <binhle030601@gmail.com>
2022-02-24 11:42:00 +0700

    up

---
+++ src/LamTheoTaiLieu/CauTrucReNhanh/IfElseExample.java
@@ -0,0 +1,18 @@
+package LamTheoTaiLieu.CauTrucReNhanh;
+
+import java.util.Scanner;
+
+public class IfElseExample {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        double score;
+        System.out.println("Nhap diem: ");
+        score = input.nextDouble();
+
+        if(score < 4.0)
+            System.out.println("Xin loi, ban da rot khoa hoc. ");
+        else
+            System.out.println("Xin chuc mung, ban da vuot qua khoa hoc. ");
+    }
+}
