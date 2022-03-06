dccf595f159b1ecd00de9d0b246425c522cc3b8d
LeDoQuocBinh_2008110078_K14DCPM01 <binhle030601@gmail.com>
2022-03-06 16:13:23 +0700

    up

---
+++ src/shapeapp/Amoeba.java
@@ -0,0 +1,29 @@
+package shapeapp;
+
+public class Amoeba {
+    
+    //attribue
+    String soundFile;
+    //toa do
+    double xPoint,yPoint;
+
+
+    //method
+    public Amoeba(String soundFile){
+        this.soundFile=soundFile;
+    }
+    public Amoeba(String soundFile, double xPoint, double yPoint){
+        this.soundFile=soundFile;
+        this.xPoint=xPoint;
+        this.yPoint=yPoint;
+    }
+
+
+    void rotate(){
+        System.out.println("Xoay xoay xoay at xPoint: "+this.xPoint + "yPoint: "+this.yPoint+this.soundFile);
+        this.playSound();
+    }
+    void playSound(){
+        System.out.println("Play "+this.soundFile);
+    }
+}
---
+++ src/shapeapp/AmoebaTestDrive.java
@@ -0,0 +1,11 @@
+package shapeapp;
+
+public class AmoebaTestDrive {
+    public static void main(String[] args) {
+        Amoeba amoeba = new Amoeba("Amoeba.hif");
+        amoeba.rotate();
+
+        Amoeba amoeba2 = new Amoeba("Amoeba.hif",5.5,7.5);
+        amoeba2.rotate();
+    }
+}
