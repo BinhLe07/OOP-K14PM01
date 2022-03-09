a0049e0430cc5163947d2632f59dfee542bff449
LeDoQuocBinh_2008110078_K14DCPM01 <binhle030601@gmail.com>
2022-03-09 23:50:29 +0700

    up

---
+++ src/oo/EmloyeeTestDrive.java
@@ -0,0 +1,8 @@
+package oo;
+
+public class EmloyeeTestDrive {
+    public static void main(String[] args) {
+        Emloyee emloyee = new Emloyee("A", "1/1", 1000);
+        System.out.println(emloyee);
+    }
+}
