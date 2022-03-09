c7bddafbabd6ca17fc5af3f67d043088a056a53f
LeDoQuocBinh_2008110078_K14DCPM01 <binhle030601@gmail.com>
2022-03-09 22:09:49 +0700

    up

---
+++ src/oo/SanPhamBai2.java
@@ -0,0 +1,23 @@
+package oo;
+
+import java.util.Scanner;
+
+public class SanPhamBai2 {
+    String tenSP;
+    double donGia;
+
+    Scanner input = new Scanner(System.in);
+
+    void nhap(){
+        System.out.print("Ten san pham: ");
+        tenSP = input.nextLine();
+        System.out.print("Gia ban: ");
+        donGia = input.nextDouble();
+    }
+    void xuat(){
+        System.out.println("Ten san pham: "+tenSP);
+        System.out.println("Gia ban: "+donGia);
+    }
+
+
+}
---
+++ src/oo/SanPhamBai2TestDrive.java
@@ -0,0 +1,16 @@
+package oo;
+
+public class SanPhamBai2TestDrive {
+    public static void main(String[] args) {
+        SanPhamBai2 sanpham1 = new SanPhamBai2();
+
+        sanpham1.nhap();
+
+        SanPhamBai2 sanpham2 = new SanPhamBai2();
+
+        sanpham2.nhap();
+
+        sanpham1.xuat();
+        sanpham2.xuat();
+    }
+}
