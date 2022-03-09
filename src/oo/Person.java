3dcdae7c6a45346bc1eea147afc48f2439604535
LeDoQuocBinh_2008110078_K14DCPM01 <binhle030601@gmail.com>
2022-03-09 23:30:20 +0700

    up

---
+++ src/oo/Person.java
@@ -0,0 +1,20 @@
+package oo;
+
+public class Person {
+    protected String name;
+    protected String birthday;
+
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+    public String getBirthday() {
+        return birthday;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+}
